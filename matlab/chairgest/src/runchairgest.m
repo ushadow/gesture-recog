4d88e228bc2bc764cb0217edc8723f3c3ba9ba45
Ying Yin <clevershadow@gmail.com>
2014-03-11 15:08:10 -0400

    Fixed a bug in segmenting gestures for training.

--- matlab/chairgest/src/runchairgest.m
+++ matlab/chairgest/src/runchairgest.m
@@ -21,9 +21,9 @@ hyperParam = hyperparamchairgest(combinedData{1}.param, 'dataFile', 'combinedDat
 jobParam = jobparam;
 
 % Test.
-R = runexperiment(hyperParam, testSplit(:, 1), 1, 1, 1, combinedData);
+%R = runexperiment(hyperParam, testSplit(:, 1), 1, 1, 1, combinedData);
 
 % Parallel run.
-% runexperimentbatch(combinedData, split, hyperParam, jobParam);
+runexperimentbatch(combinedData, split, hyperParam, jobParam);
 
 %outputchairgest(dsKinectXsens{1}, job247_output, 'hmm-nM-6-247-1session1user', 'yingyin', gesturelabel)
--- matlab/meet/learn/ldcrf/trainldcrfwrap.m
+++ matlab/meet/learn/ldcrf/trainldcrfwrap.m
@@ -31,13 +31,15 @@ for i = 1 : nSeq
   runs = runs{1, 2};
   for j = 1 : size(runs, 1)
     run = runs(j, :);
-    newX{end + 1} = seqX(:, run(1) : run(2)); %#ok<AGROW>
     label = seqY(run(1) : run(2)); 
     I = find(label <= nGesture);
-    gestureLabel = label(I(1));
-    label(label == nGesture + 1) = nGesture + (gestureLabel - 1) * 2 + 1;
-    label(label == nGesture + 2) = nGesture + gestureLabel * 2;
-    newY{end + 1} = label; %#ok<AGROW>
+    if numel(I) > 0
+      gestureLabel = label(I(1));
+      label(label == nGesture + 1) = nGesture + (gestureLabel - 1) * 2 + 1;
+      label(label == nGesture + 2) = nGesture + gestureLabel * 2;
+      newY{end + 1} = label; %#ok<AGROW>
+      newX{end + 1} = seqX(:, run(1) : run(2)); %#ok<AGROW>
+    end
   end
 end
 end
