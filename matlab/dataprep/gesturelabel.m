a2d694442c56354e43c38702d9f309b21c223ab9
Ying Yin <clevershadow@gmail.com>
2014-04-24 19:28:13 -0400

    Removed Rest definition in the file and added it to the matlab script.

--- matlab/dataprep/gesturelabel.m
+++ matlab/dataprep/gesturelabel.m
@@ -12,15 +12,19 @@ function [label, dict, type, repeat, nS, nHandPoseType] = gesturelabel(gestureDe
 data = importdata(fullfile(gestureDefDir, 'gesture_def.txt'), ...
                   ',', 1);
 label = data.textdata(2 : end, 1);
+label{end + 1} = 'Rest';
 label{end + 1} = 'Other'; % small movement without nucleus
 label{end + 1} = 'OtherPose'; % wrong gesture should be ignored
 
 type = data.textdata(2 : end, 2);
+type{end + 1} = 'R'; % Rest
 type{end + 1} = 'O'; % Other
 type{end + 1} = 'OP'; % OtherPose
 
 repeat = data.data(:, 1);
+repeat(end + 1) = 0; % Rest
 nS = data.data(:, 2);
+nS(end + 1) = 1; % Rest
 dict = containers.Map(label, 1 : length(label));
 
 nHandPoseType = sum(strcmp(type, 'S')) + 1;
