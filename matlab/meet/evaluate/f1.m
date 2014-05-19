2b6ba4c61ab10da2ea434e96bf4f1de956974d15
Ying Yin <clevershadow@gmail.com>
2014-05-19 18:46:22 -0400

    Make a function as an individual file.

--- matlab/meet/evaluate/f1.m
+++ matlab/meet/evaluate/f1.m
@@ -128,16 +128,4 @@ timeDiff = (gtRange(2) - gtRange(1) + 1) / 2;
 res = range(2) >= gtRange(1) && range(2) < gtRange(2) + timeDiff;
 end
 
-function segByClass = computegtsegments(Y, restLabel)
-ndx = find(Y(2, :) == 2);
-startNdx = 1;
-segByClass = cell(1, restLabel - 1);
-for j = 1 : length(ndx)
-  endNdx = ndx(j);
-  gestureLabel = Y(1, endNdx);
-  if gestureLabel < restLabel
-    segByClass{gestureLabel} = [segByClass{gestureLabel}; [startNdx endNdx]];
-  end
-  startNdx = endNdx + 1;
-end
-end
+
